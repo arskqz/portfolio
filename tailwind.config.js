ac89769af2e468cedd8ba0f6b635dae2d543a023
arskqz <aaro.s.turunen@outlook.com>
2025-04-30 12:21:17 +0300

    Config and main

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Source": ["Source Code Pro", "monospace"]
+      }
+    },
+  },
+  plugins: [],
+}
+
